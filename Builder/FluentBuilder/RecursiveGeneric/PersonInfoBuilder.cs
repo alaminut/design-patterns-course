864d41280f74f9c85810f1d92f3b7bfa23044814
Hakan Sulun <hakan@core-attacks.com>
2019-05-27 13:55:01 +0300

    Fixed Recursive Generic Builder Pattern Implementation
    Removed generic type parameter from abstract base class as it  was not necessary for implementation itself.

--- Builder/FluentBuilder/RecursiveGeneric/PersonInfoBuilder.cs
+++ Builder/FluentBuilder/RecursiveGeneric/PersonInfoBuilder.cs
@@ -1,6 +1,6 @@
 namespace Builder.FluentBuilder.RecursiveGeneric
 {
-    public class PersonInfoBuilder<TSelf> : PersonBuilder<PersonInfoBuilder<TSelf>>
+    public class PersonInfoBuilder<TSelf> : PersonBuilder
         where TSelf : PersonInfoBuilder<TSelf>
     {
         public TSelf WithName(string name)
