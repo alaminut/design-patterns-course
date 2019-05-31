5e85014aedadb3f349d48824b9f07de95fc22f96
Hakan Sulun <3727398+alaminut@users.noreply.github.com>
2019-06-01 02:13:47 +0300

    Interpreter Pattern Implementation

---
+++ Interpreter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Interpreter.Exercise;
+using static System.Console;
+
+namespace Interpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = "1+23+c";
+            var p = new ExpressionProcessor();
+            p.Variables.Add('c', 6);
+            WriteLine(p.Calculate(input));
+        }
+    }
+}
